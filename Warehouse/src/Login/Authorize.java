a99f20e8019ace646d3d85d9f1b540a76c0a30af
Giaro <koutsioubasgiaro@gmail.com>
2018-01-23 17:40:00 +0200

    gui on login v0.1

--- Warehouse/src/Login/Authorize.java
+++ Warehouse/src/Login/Authorize.java
@@ -20,6 +20,9 @@ import javax.swing.JTextField;
 import gui.gui_test;
 
 import javax.swing.JButton;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import java.awt.Font;
 
 
 public class Authorize extends JFrame implements ActionListener
@@ -30,62 +33,41 @@ public class Authorize extends JFrame implements ActionListener
 	
 	/* Setting the frame */
 	public Authorize() 
-	{	
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		getContentPane().setLayout(gridBagLayout);
-		setBounds(100, 100, 450, 300);
+	{
+		setTitle("Login");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Giaro\\Desktop\\SELF-Project\\Software-Engineering-Lab-Final-Project\\Warehouse\\src\\selfp\\box_PNG49.png"));
+		setBounds(100, 100, 300, 200);
 		setResizable(false);
+		getContentPane().setLayout(null);
 		
 		JLabel username_label = new JLabel("UserName");
-		GridBagConstraints gbc_username_label = new GridBagConstraints();
-		gbc_username_label.anchor = GridBagConstraints.WEST;
-		gbc_username_label.insets = new Insets(0, 0, 5, 5);
-		gbc_username_label.gridx = 1;
-		gbc_username_label.gridy = 1;
-		getContentPane().add(username_label, gbc_username_label);
+		username_label.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
+		username_label.setBounds(40, 30, 100, 30);
+		getContentPane().add(username_label);
 		
 		username_textfield = new JTextField();
-		GridBagConstraints gbc_username_textfield = new GridBagConstraints();
-		gbc_username_textfield.anchor = GridBagConstraints.WEST;
-		gbc_username_textfield.insets = new Insets(0, 0, 5, 0);
-		gbc_username_textfield.gridx = 3;
-		gbc_username_textfield.gridy = 1;
-		getContentPane().add(username_textfield, gbc_username_textfield);
+		username_textfield.setBounds(150, 30, 100, 30);
+		getContentPane().add(username_textfield);
 		username_textfield.setColumns(10);
 		
 		JLabel password_label = new JLabel("Password");
-		GridBagConstraints gbc_password_label = new GridBagConstraints();
-		gbc_password_label.insets = new Insets(0, 0, 5, 5);
-		gbc_password_label.gridx = 1;
-		gbc_password_label.gridy = 3;
-		getContentPane().add(password_label, gbc_password_label);
+		password_label.setFont(new Font("Copperplate Gothic Bold", Font.PLAIN, 15));
+		password_label.setBounds(40, 72, 100, 30);
+		getContentPane().add(password_label);
 		
 		password_textfield = new JTextField();
-		GridBagConstraints gbc_password_textfield = new GridBagConstraints();
-		gbc_password_textfield.insets = new Insets(0, 0, 5, 0);
-		gbc_password_textfield.anchor = GridBagConstraints.WEST;
-		gbc_password_textfield.gridx = 3;
-		gbc_password_textfield.gridy = 3;
-		getContentPane().add(password_textfield, gbc_password_textfield);
+		password_textfield.setBounds(150, 71, 100, 30);
+		getContentPane().add(password_textfield);
 		password_textfield.setColumns(10);
 		
 		JButton sign_in_btn = new JButton("Sign-In");
-		GridBagConstraints gbc_sign_in_btn = new GridBagConstraints();
-		gbc_sign_in_btn.insets = new Insets(0, 0, 5, 0);
-		gbc_sign_in_btn.anchor = GridBagConstraints.WEST;
-		gbc_sign_in_btn.gridx = 3;
-		gbc_sign_in_btn.gridy = 5;
-		getContentPane().add(sign_in_btn, gbc_sign_in_btn);
+		sign_in_btn.setBounds(100, 135, 100, 25);
+		getContentPane().add(sign_in_btn);
 		
 		JLabel Credential_Info = new JLabel("");
-		GridBagConstraints gbc_Credential_Info = new GridBagConstraints();
-		gbc_Credential_Info.gridx = 3;
-		gbc_Credential_Info.gridy = 6;
-		getContentPane().add(Credential_Info, gbc_Credential_Info);
+		Credential_Info.setIcon(new ImageIcon("C:\\Users\\Giaro\\Desktop\\SELF-Project\\Software-Engineering-Lab-Final-Project\\Warehouse\\src\\selfp\\box.jpg"));
+		Credential_Info.setBounds(0, 0, 300, 200);
+		getContentPane().add(Credential_Info);
 		
 		sign_in_btn.addActionListener(this); // attach a listener for the sign-in button
 	}
